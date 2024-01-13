0390154369c39941755357f5aa0038aca7f0fa9c
nasbf <mo0ma@hotmail.com>
2024-01-12 19:07:15 -0500

    change prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine ();
+        Console.WriteLine("what is your last name? ");
+        string lname = Console.ReadLine ();
+        Console.WriteLine($"your name is {lname}, {name} {lname} ");
     }
 }
