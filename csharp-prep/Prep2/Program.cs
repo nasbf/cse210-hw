4bd92fc5e95e525f6537716309afcbb0426d0380
nasbf <mo0ma@hotmail.com>
2024-01-12 21:33:33 -0500

    activity prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        int grade = int.Parse (number);
+
+        string letter = " ";
+
+        if (grade >= 90)
+        {
+            letter =  "A";
+        }
+        else if (grade >= 80)
+        {
+            letter =  "B";
+        }
+        else if (grade >= 70)
+        {
+            letter =  "C";
+        }
+        else if (grade >= 60)
+        {
+            letter =  "D";
+        }
+        else
+        {
+            letter =  "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations you pass this course");
+        }
+        else
+        {
+            Console.WriteLine("next time you'll achieve this goal ");
+        }
     }
 }
